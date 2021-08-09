b1a9ad113627b3056cb80655e70bf1b39cb850d0
noLePhi <philipperambaud.ai@gmail.com>
2021-08-09 15:08:23 +0200

    add game and agent scripts to start RL

---
+++ agent.py
@@ -0,0 +1,23 @@
+import torch
+import random
+import numpy as np
+from collections import deque
+from game import SnakeGameAI, Direction, Point
+
+MAX_MEMORY = 100_000
+BATCH_SIZE = 100
+LR = 0.001
+
+class Agent:
+
+	def __init__(self):
+		pass
+
+	def get_state(self, game):
+		pass
+
+	def remember(self, state, action, reward, next_state, done):
+		pass
+
+
+	def train_
